2e066c351379f25b829ddeaa93dea20010c6bb35
tamircroll <tamircroll@gmail>
2015-04-19 00:48:09 +0300

    [Feature] - new class to Q2

---
+++ B15_Ex01_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace B15_Ex01_2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}

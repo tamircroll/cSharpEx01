0d255cfac932341d5828a922f38e1954e76af3bc
tamircroll <tamircroll@gmail>
2015-04-18 21:44:02 +0300

    [Feature] - Reading numbers from user at question 1

--- B15_Ex01_1/Program.cs
+++ B15_Ex01_1/Program.cs
@@ -6,11 +6,36 @@
 
 namespace B15_Ex01_1
 {
-     public class Program
+    public class Program
     {
-         static void Main()
-         {
+        public static void Main()
+        {
+            string[] numbersStr = new string[5];
+            int[] numbers;
 
-         }
+            numbers = readNumbersFromUser(5, 3);
+
+
+        }
+
+
+        private static int[] readNumbersFromUser(int numOfNumbersToRead, int numberOfDigits)
+        {
+            System.Console.WriteLine("Please enter " + numOfNumbersToRead + " numbers with " + numberOfDigits + " digits each");
+            string numberStr = "";
+            int[] numbers = new int[numOfNumbersToRead];
+            for (int i = 0; i < numOfNumbersToRead ; i++)
+            {
+                numberStr = System.Console.ReadLine();
+                bool goodInput = (int.TryParse(numberStr , out numbers[i])) && (numberStr.Length == 3);
+                if (!goodInput)
+                {
+                    System.Console.WriteLine("The input you entered is invalid. Please try again.");
+                    i--;
+                }
+                
+            }
+            return numbers;
+        }
     }
 }

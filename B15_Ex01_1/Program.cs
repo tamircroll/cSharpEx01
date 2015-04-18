f446d9519a4419d5ead71001d4afe321b4af268e
tamircroll <tamircroll@gmail>
2015-04-18 20:29:18 +0300

    First commit

---
+++ B15_Ex01_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace B15_Ex01_1
+{
+     public class Program
+    {
+         static void Main()
+         {
+
+         }
+    }
+}

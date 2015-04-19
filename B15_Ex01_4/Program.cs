77508e569f203b2f02ea299eec6ede76076a7c04
tamircroll <tamircroll@gmail>
2015-04-19 04:12:18 +0300

    [Feature] - Q4 - almost done - need a check for the palindrome..

--- B15_Ex01_4/Program.cs
+++ B15_Ex01_4/Program.cs
@@ -8,8 +8,8 @@
     {
         public static void Main()
         {
-            bool v_IsNumber, v_IsString, v_isSizeTen;
-            int i_number;
+            bool v_IsNumber;
+            long io_InputInt;
 
             Console.WriteLine("Please enter a string with 10 letters or a number with 10 digits:");
             string io_Input = Console.ReadLine();
@@ -18,52 +18,92 @@
                 Console.WriteLine("Invalid input, please try again:");
                 io_Input = Console.ReadLine();
             }
-            isPolindrom(io_Input);
-            v_IsNumber = int.TryParse(io_Input, out i_number);
+
+            printIsPolindrom(io_Input);
+            v_IsNumber = long.TryParse(io_Input, out io_InputInt);
             if (v_IsNumber)
             {
-                sumDigits();
+                sumDigits(io_InputInt);
             }
             else
             {
-                sumCamelCase();
+                sumCamelCase(io_Input);
             }
 
+            System.Console.WriteLine("Please press 'Enter' to exit");
+            System.Console.ReadLine();
         }
 
-        private static void sumCamelCase()
+        private static void sumCamelCase(string i_Input)
         {
-            throw new NotImplementedException();
+            int i_SumCamel = 0;
+            char[] i_inputAsCharArray = i_Input.ToCharArray();
+            for (int i = 0; i < i_inputAsCharArray.Length; i++ )
+            {
+                char i_CurLetter = i_inputAsCharArray[i];
+                if (i_CurLetter >= 'A' && i_CurLetter <= 'Z')
+                {
+                    i_SumCamel++;
+                }
+            }
+            Console.WriteLine("The number of camel case letter is: " + i_SumCamel);
         }
 
-        private static void sumDigits()
+        private static void sumDigits(long i_number)
         {
-            throw new NotImplementedException();
+            long i_Sum = 0;
+            while(i_number > 0)
+            {
+                i_Sum += i_number % 10;
+                i_number /= 10;
+            }
+            Console.WriteLine("The sum of all digits is: " + i_Sum);
         }
 
-        private static void isPolindrom(string io_Input)
+        private static void printIsPolindrom(string i_Input)
+        {   
+            bool v_IsPolindrom = true;
+            char[] i_inputAsCharArray = i_Input.ToCharArray();
+            for (int i = 0; i < i_inputAsCharArray.Length / 2; i++)
+            {
+                if (i_inputAsCharArray[i] != i_inputAsCharArray[i_inputAsCharArray.Length - i - 1])
+                {
+                    v_IsPolindrom = false;
+                }
+            }
+            if (v_IsPolindrom)
+            {
+                Console.WriteLine("The input is a polindrom");
+            }
+            else
+            {
+                Console.WriteLine("The input is not a polindrom");
+            }
+        }
+
+        private static void printIsPolindrom(int i_Input)
         {
-            throw new NotImplementedException();
+            printIsPolindrom(i_Input.ToString());
         }
 
-        private static bool isValidInput(string i_input)
+        private static bool isValidInput(string i_Input)
         {
             int i_number;
-            bool v_isSizeTen = i_input.Length == 10;
-            bool v_IsNumber = int.TryParse(i_input, out i_number);
-            bool v_IsString = IsOnlyLetters(i_input);
+            bool v_isSizeTen = i_Input.Length == 10;
+            bool v_IsNumber = int.TryParse(i_Input, out i_number);
+            bool v_IsString = IsOnlyLetters(i_Input);
 
             return (!v_IsNumber || !v_IsString) && !v_isSizeTen;
         }
 
-        private static bool IsOnlyLetters(string i_input)
+        private static bool IsOnlyLetters(string i_Input)
         {
             bool v_IsOnlyLetters = true;
+            char[] i_inputAsCharArray = i_Input.ToCharArray();
 
-            char[] i_inputCharArray = i_input.ToCharArray();
-            for (int i = 0; i < i_inputCharArray.Length; i++)
+            for (int i = 0; i < i_inputAsCharArray.Length; i++)
             {
-                if (i_inputCharArray[i] < Char.GetNumericValue('a') || i_inputCharArray[i] > Char.GetNumericValue('Z'))
+                if (i_inputAsCharArray[i] < 'a' || i_inputAsCharArray[i] > 'Z')
                 {
                     v_IsOnlyLetters = false;
                 }
